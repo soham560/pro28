889eb161d6efbb48c13a93ca7be683d289609464
soham560 <77228631+soham560@users.noreply.github.com>
2021-05-19 12:40:55 +0530

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,15 @@
+class Stone extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("sprites/stone.png");
+    }
+  
+    display() {
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    }
+       fly(){
+      this.sling.bodyA = null;
+       }
+      }
---
+++ Tree.js
@@ -0,0 +1,7 @@
+class Tree extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/tree.png");
+    }
+  
+  };
